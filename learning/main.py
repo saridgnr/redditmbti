1d1860aefd947682d13a9391a7519e3caab52173
Omer Sarid <saridgnr@gmail.com>
2019-03-11 03:13:11 +0200

    before big data

--- learning/main.py
+++ learning/main.py
@@ -8,21 +8,21 @@ import random
 import torch.nn.functional as F
 from random import shuffle
 from sklearn.metrics import precision_recall_fscore_support
-
+from os import path
 from emo_model import EmoModel
 from vocab import Vocab
 
-MAX_LENGTH = 100
+MAX_LENGTH = 500
 PERSONALITY = "personality"
 REDDIT_SCORE = "reddit_score"
 CONTENT_1 = "content_1"
 CONTENT_2 = "content_2"
 
-nlp = sp.load('en')
+nlp = sp.load('en', disable=["tagger", "parser", "ner"])
 
 
 def main():
-    data = pd.read_csv(r"D:\Users\White\Desktop\Code\colman\NLP\redditMBTI\scrapping\data_100\mbti.tsv",
+    data = pd.read_csv(r"D:\Users\White\Desktop\Code\colman\NLP\redditMBTI\scrapping\redditMBTIBig.tsv",
                        sep="\t",
                        header=None,
                        names=[PERSONALITY, REDDIT_SCORE, CONTENT_1, CONTENT_2],
@@ -43,14 +43,15 @@ def main():
     n_layers = 2
     hidden_size = 800
     embedding_size = 300
-
-    model = EmoModel(vocab.word_count, hidden_size, embedding_size, vocab.tag_count, n_layers).cuda()
-
+    if not path.exists("model.pth"):
+        model = EmoModel(vocab.word_count, hidden_size, embedding_size, vocab.tag_count, n_layers).cuda()
+    else:
+        print("model found! loading...")
+        torch.load("model.pth")
     criterion = nn.CrossEntropyLoss()
     learning_rate = 0.0001
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-    n_epochs = 10000
+    n_epochs = 1000000
     print_every = 30
     sample_every = 50
     eval_every = 2500
@@ -77,6 +78,7 @@ def main():
 
 def build_dataset(data, vocab):
     instances = []
+    print("building the dataset")
     for index, row in data.iterrows():
         for content in {CONTENT_1, CONTENT_2}:
             text = row.get(content, None)
@@ -108,6 +110,9 @@ def train_sentence(sentence, target, criterion, model, optimizer):
 
 
 def eval(model, test):
+    print("saving the model")
+    torch.save(model, "model.pth")
+    print("evaluate the model")
     y_true = []
     y_predicted = []
     for t in test:
