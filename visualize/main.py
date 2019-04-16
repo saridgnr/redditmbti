25ace73691c9f8fafbeae96b1765db2aeaf113f2
Omer Sarid <saridgnr@gmail.com>
2019-04-16 03:30:32 +0300

    results

--- visualize/main.py
+++ visualize/main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    run_data_path = r"D:\Users\White\Desktop\Code\colman\NLP\redditMBTI\learning\1554079514.152244"
+    run_data_path = r"D:\Users\White\Desktop\Code\colman\NLP\redditMBTI\learning\1555199106.287743-450embd-800hidden-1layer"
     with open(run_data_path, "r") as f:
         epochs_raw = {int(line.split(",", 1)[0]): line.split(",", 1)[1].rstrip() for line in f.readlines()}
         epochs = {}
@@ -12,15 +12,20 @@ def main():
             for d in filter(None, data.split("|")[1:]):
                 epochs[num][d.split(":")[0]] = tuple(map(float, d.split(":")[1].split(",")[:-1]))
     ordered_epochs = sorted(epochs.items(), key=lambda x: x[0])
-    #plt.plot([x[0] for x in ordered_epochs], [x[1]["loss"] for x in ordered_epochs])
-    #plt.plot([x[0] for x in ordered_epochs], [x[1]["EI"][0] for x in ordered_epochs])
-    #plt.plot([x[0] for x in ordered_epochs], [x[1]["EI"][1] for x in ordered_epochs])
-    #plt.plot([x[0] for x in ordered_epochs], [x[1]["EI"][2] for x in ordered_epochs])
+    keys = ("loss", "all", "EI", "SN", "TF", "JP")
+    for key in keys:
+        if key == "loss":
+            plt.plot([x[0] for x in ordered_epochs], [x[1]["loss"] for x in ordered_epochs])
+        else:
+            plt.plot([x[0] for x in ordered_epochs], [x[1][key][0] for x in ordered_epochs])
+            plt.plot([x[0] for x in ordered_epochs], [x[1][key][1] for x in ordered_epochs])
+            plt.plot([x[0] for x in ordered_epochs], [x[1][key][2] for x in ordered_epochs])
 
+        plt.xlabel('Epochs')
+        plt.suptitle(key, fontsize=14)
 
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.show()
+        #plt.ylabel('Loss')
+        plt.show()
 
 if __name__== "__main__":
     main()
